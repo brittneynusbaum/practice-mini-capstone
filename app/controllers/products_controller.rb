0b8970d1d381df6e7533af7ab944c8685cae9a34
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-21 23:32:58 -0700

    adds index method & requests.http

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,7 @@
 class ProductsController < ApplicationController
+
+  def index
+    render json: {message: "testing index"}
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get "/products" => "products/index"
+  get "/products" => "products#index"
   
 end
